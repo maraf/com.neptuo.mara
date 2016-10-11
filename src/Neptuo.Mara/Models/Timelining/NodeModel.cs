cff0b9509d3dcc61ce5622141242946376482463
maraf <mara@neptuo.com>
2016-10-11 18:57:15 +0200

    Description language.

--- src/Neptuo.Mara/Models/Timelining/NodeModel.cs
+++ src/Neptuo.Mara/Models/Timelining/NodeModel.cs
@@ -12,6 +12,6 @@ namespace Neptuo.Mara.Models.Timelining
     {
         public DateTime When { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
+        public TextModel Description { get; set; }
     }
 }
