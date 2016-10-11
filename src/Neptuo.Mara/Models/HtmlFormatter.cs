aaa84f2a5575a9bb7a363a779986aa11a91f8ed8
maraf <mara@neptuo.com>
2016-10-11 19:13:48 +0200

    Enriched timeline nodes. Add support <br /> in markdown for single line enter.

---
+++ src/Neptuo.Mara/Models/HtmlFormatter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using CommonMark;
+using CommonMark.Syntax;
+using Skybrud.SyntaxHighlighter;
+
+namespace Neptuo.Mara.Models
+{
+    public class HtmlFormatter : CommonMark.Formatters.HtmlFormatter
+    {
+        public HtmlFormatter(TextWriter target, CommonMarkSettings settings)
+            : base(target, settings)
+        { }
+
+        protected override void WriteBlock(Block block, bool isOpening, bool isClosing, out bool ignoreChildNodes)
+        {
+            string content;
+            if (block.Tag == BlockTag.FencedCode && TryFormatCode(block.FencedCodeData.Info, block.StringContent, out content))
+            {
+                Write(content);
+                ignoreChildNodes = true;
+            }
+            else
+            {
+                base.WriteBlock(block, isOpening, isClosing, out ignoreChildNodes);
+            }
+        }
+
+        protected override void WriteInline(Inline inline, bool isOpening, bool isClosing, out bool ignoreChildNodes)
+        {
+            base.WriteInline(inline, isOpening, isClosing, out ignoreChildNodes);
+
+            if (inline.Tag == InlineTag.SoftBreak)
+                Write("<br />");
+        }
+
+        private bool TryFormatCode(string language, StringContent content, out string result)
+        {
+            string source = content.ToString();
+            switch (language.ToLowerInvariant())
+            {
+                case "xml":
+                    result = Highlighter.HighlightXml(source);
+                    return true;
+                case "javascript":
+                case "js":
+                    result = Highlighter.HighlightJavaScript(source);
+                    return true;
+                case "json":
+                    result = Highlighter.HighlightJson(source);
+                    return true;
+                case "csharp":
+                case "c#":
+                    result = Highlighter.HighlightCSharp(source);
+                    return true;
+            }
+
+            result = null;
+            return false;
+        }
+    }
+}
--- src/Neptuo.Mara/Models/TextModel.cs
+++ src/Neptuo.Mara/Models/TextModel.cs
@@ -1,5 +1,4 @@
 ﻿using CommonMark;
-using CommonMark.Formatters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +26,17 @@ namespace Neptuo.Mara.Models
                     CommonMarkSettings.Default.OutputDelegate = (doc, output, settings) =>
                         new HtmlFormatter(output, settings).WriteDocument(doc);
 
-                    string html = CommonMarkConverter.Convert(Content);
+                    string content = Content;
+                    if (string.IsNullOrEmpty(content))
+                        return null;
+
+                    IEnumerable<string> lines = content
+                        .Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+                    lines = lines.Select(l => l.TrimStart());
+                    content = String.Join(Environment.NewLine, lines);
+
+                    string html = CommonMarkConverter.Convert(content);
                     return html;
                 default:
                     throw new NotSupportedException(Language.ToString());
